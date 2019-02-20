7710e73a9388eaaf2666826b14c800125a872c32
Alan Witkowski <alan.witkowski@gmail.com>
2019-02-19 20:19:06 -0700

    updated license year

--- src/objects/player.h
+++ src/objects/player.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * irrlamb - https://github.com/jazztickets/irrlamb
-* Copyright (C) 2015  Alan Witkowski
+* Copyright (C) 2019  Alan Witkowski
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
