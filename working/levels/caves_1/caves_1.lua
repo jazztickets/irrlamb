ff76cdf41ffe7dd187014e2989e0bb1c7c3b349a
Alan Witkowski <alan.witkowski@gmail.com>
2019-02-21 20:31:55 -0700

    worked on caves_1

--- working/levels/caves_1/caves_1.lua
+++ working/levels/caves_1/caves_1.lua
@@ -9,3 +9,12 @@ end
 
 -- Set up goal
 GoalCount = 5
+
+-- Build block stack
+tRubble = Level.GetTemplate("rubble")
+
+for i = 0, 2 do
+	for j = 0, 4 do
+		Level.CreateObject("rubble", tRubble, -27 + j * 0.5, 15, 76 + i * 0.5)
+	end
+end
