d53e23558bb19cf75df9ea7bee72b01370d81b1b
Alan Witkowski <alan.witkowski@gmail.com>
2019-02-25 15:50:23 -0700

    worked on skate_2

---
+++ working/levels/skate_2/skate_2.lua
@@ -0,0 +1,14 @@
+
+-- Called when an orb is deactivated
+function OnOrbDeactivate()
+	GoalCount = GoalCount - 1
+	if GoalCount == 0 then
+		Level.Win()
+	end
+end
+
+-- Set up goal
+GoalCount = 5
+
+-- Set up level
+--Camera.SetYaw(0)
