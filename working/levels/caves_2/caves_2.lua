c4c20de6c82a0a376878b33fe78c8836b323857f
Alan Witkowski <alan.witkowski@gmail.com>
2019-02-25 09:47:07 -0700

    worked on caves_2

--- working/levels/caves_2/caves_2.lua
+++ working/levels/caves_2/caves_2.lua
@@ -2,13 +2,30 @@
 -- Called when an orb is deactivated
 function OnOrbDeactivate()
 	GoalCount = GoalCount - 1
-	if GoalCount == 0 then
+	if GoalCount == 1 then
+		ShowLastOrb()
+	elseif GoalCount == 0 then
 		Level.Win()
 	end
 end
 
+-- Spawn last orb
+function ShowLastOrb()
+	tOrb = Level.GetTemplate("orb")
+	oOrb = Level.CreateObject("orb", tOrb, 0, 8, 80, 0, 0, 0)
+
+	GUI.TutorialText("At last! Now exit the cave.", 5)
+end
+
+-- Display lose message
+function OnHitZone(HitType, Zone, HitObject)
+	if HitObject == Player then
+		Level.Lose("You fell into hot lava!")
+		return 1
+	end
+
+	return 0
+end
+
 -- Set up goal
 GoalCount = 5
-
--- Angle camera down
---Camera.SetPitch(75)
