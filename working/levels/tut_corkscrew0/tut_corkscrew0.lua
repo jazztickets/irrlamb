08ec00bc39d6b91b566315c9eb9dc77b3f93e81e
Alan Witkowski <alan.witkowski@gmail.com>
2019-02-27 21:51:35 -0700

    added corkscrew tutorial

---
+++ working/levels/tut_corkscrew0/tut_corkscrew0.lua
@@ -0,0 +1,14 @@
+
+-- Called when an orb is deactivated
+function OnOrbDeactivate()
+	GoalCount = GoalCount - 1
+	if GoalCount == 0 then
+		Level.Win()
+	end
+end
+
+-- Set up goal
+GoalCount = 1
+
+-- Show text
+GUI.TutorialText("Start holding [" .. KEY_LEFT .. "] as you climb up the ramp. This will move you to the right when you're upside down.", 10)
