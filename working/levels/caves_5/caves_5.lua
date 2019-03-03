4cdd3fe13804945d41b5df84086fa0cda27b3141
Alan Witkowski <alan.witkowski@gmail.com>
2019-03-03 09:17:19 -0700

    finished caves_5

--- working/levels/caves_5/caves_5.lua
+++ working/levels/caves_5/caves_5.lua
@@ -10,6 +10,11 @@ end
 function OnHitPlayer(PlayerObject, OtherObject)
 	if OtherObject == oLava then
 		Level.Lose("You fell into hot lava!")
+	else
+		Template = Object.GetTemplate(OtherObject)
+		if Template == tOrb then
+			Object.SetLifetime(OtherObject, 25)
+		end
 	end
 end
 
@@ -18,6 +23,7 @@ GoalCount = 10
 
 -- Get objects
 oLava = Object.GetPointer("lava")
+tOrb = Level.GetTemplate("orb")
 
 -- Set up camera
 Camera.SetYaw(45)
