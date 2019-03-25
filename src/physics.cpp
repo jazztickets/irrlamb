f0ddb381b9e5c984c8d7cbd4379fb658a2ea7852
Alan Witkowski <alan.witkowski@gmail.com>
2019-03-25 11:26:02 -0600

    clean up

--- src/physics.cpp
+++ src/physics.cpp
@@ -50,29 +50,23 @@ static void RayCallback(void *Data, dGeomID Geometry1, dGeomID Geometry2) {
 }
 
 // Near collision callback
-static void ODECallback(void *Data, dGeomID Geometry1, dGeomID Geometry2) {
+static void ODECallback(void *Data, dGeomID Geometry, dGeomID OtherGeometry) {
 	std::vector<_ObjectCollision> *ObjectCollisions = (std::vector<_ObjectCollision> *)Data;
-	dBodyID Body1 = dGeomGetBody(Geometry1);
-	dBodyID Body2 = dGeomGetBody(Geometry2);
+	dBodyID Body = dGeomGetBody(Geometry);
+	dBodyID OtherBody = dGeomGetBody(OtherGeometry);
 
-	// Get object1
-	_Object *Object1 = nullptr;
-	if(Geometry1)
-		Object1 = (_Object *)dGeomGetData(Geometry1);
-
-	// Get object2
-	_Object *Object2 = nullptr;
-	if(Geometry2)
-		Object2 = (_Object *)dGeomGetData(Geometry2);
+	// Get objects
+	_Object *Object =(_Object *)dGeomGetData(Geometry);
+	_Object *OtherObject = (_Object *)dGeomGetData(OtherGeometry);
 
 	// Get contacts
 	dContact Contacts[MAX_CONTACTS];
-	int Count = dCollide(Geometry1, Geometry2, MAX_CONTACTS, &Contacts[0].geom, sizeof(dContact));
+	int Count = dCollide(Geometry, OtherGeometry, MAX_CONTACTS, &Contacts[0].geom, sizeof(dContact));
 	for(int i = 0; i < Count; i++) {
 
 		// Test for zones
 		bool Response = true;
-		if(Object1->GetTemplate()->CollisionGroup & _Physics::FILTER_ZONE || Object2->GetTemplate()->CollisionGroup & _Physics::FILTER_ZONE)
+		if(Object->GetTemplate()->CollisionGroup & _Physics::FILTER_ZONE || OtherObject->GetTemplate()->CollisionGroup & _Physics::FILTER_ZONE)
 			Response = false;
 
 		// Create contact joins
@@ -83,19 +77,15 @@ static void ODECallback(void *Data, dGeomID Geometry1, dGeomID Geometry2) {
 			Contacts[i].surface.rho2 = 0.001;
 
 			dJointID Joint = dJointCreateContact(Physics.GetWorld(), Physics.GetContactGroup(), &Contacts[i]);
-			dJointAttach(Joint, Body1, Body2);
+			dJointAttach(Joint, Body, OtherBody);
 		}
 
 		// Get normal
 		glm::vec3 Normal(Contacts[i].geom.normal[0], Contacts[i].geom.normal[1], Contacts[i].geom.normal[2]);
 
-		// Handle object1 collision callback
-		if(Object1)
-			ObjectCollisions->push_back(_ObjectCollision(Object1, Object2, Normal, 1));
-
-		// Handle object2 collision callback
-		if(Object2)
-			ObjectCollisions->push_back(_ObjectCollision(Object2, Object1, Normal, -1));
+		// Handle collision callback
+		ObjectCollisions->push_back(_ObjectCollision(Object, OtherObject, Normal, 1));
+		ObjectCollisions->push_back(_ObjectCollision(OtherObject, Object, Normal, -1));
 	}
 }
 
