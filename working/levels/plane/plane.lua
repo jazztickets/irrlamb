999ef38b980c64cf37b0186a657d62d366c166ab
Alan Witkowski <alan.witkowski@gmail.com>
2019-04-18 09:24:32 -0600

    worked on plane

---
+++ working/levels/plane/plane.lua
@@ -0,0 +1,21 @@
+
+-- Display zone message
+function OnHitZone(HitType, Zone, HitObject)
+
+	if HitObject == Player then
+		Level.Win()
+	end
+
+	return 0
+end
+
+-- Set up templates
+tBox = Level.GetTemplate("box")
+
+-- Build pillars
+for i = 0, 1000 do
+	z = i * 1000 + 50 + i
+	Level.CreateObject("box", tBox, -10, 25, z)
+	Level.CreateObject("box", tBox, 10, 25, z)
+end
+
