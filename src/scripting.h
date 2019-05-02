fd33572041b20d9d8caa9bc42f0e021baab03e95
Alan Witkowski <alan.witkowski@gmail.com>
2019-05-01 21:01:02 -0600

    added lua to project

--- src/scripting.h
+++ src/scripting.h
@@ -16,7 +16,7 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *******************************************************************************/
 #pragma once
-#include <lua.hpp>
+#include <lua/lua.hpp>
 #include <list>
 #include <string>
 #include <map>
